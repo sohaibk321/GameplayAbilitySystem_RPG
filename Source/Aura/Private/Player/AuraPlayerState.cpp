06ab87fc5cf0ee6e3cfdb8b9855b165b48f2f37a
Sohaib Khuram <sohaibk321@gmail.com>
2025-02-19 19:28:34 -0700

    constructing the ability system component and the attribute set to the AuraEnemy class and the AuraPlayerState class

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -3,8 +3,22 @@
 
 #include "Player/AuraPlayerState.h"
 
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
 AAuraPlayerState::AAuraPlayerState()
 {
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+
+	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
 	// how often the server will try to update clients; higher number = checks for updates more often
-	NetUpdateFrequency = 100.f; 
+	NetUpdateFrequency = 100.f;
+
+
+}
+
+UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
 }
